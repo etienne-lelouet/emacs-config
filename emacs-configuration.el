eae4096249def332954a4a4258c78b17d99311e3
Etienne Le Louet <etienne.le-louet@lip6.fr>
2023-01-24 15:05:01 +0100

    fixed mouse click behaviour in dired

--- emacs-configuration.el
+++ emacs-configuration.el
@@ -9,6 +9,10 @@
 (bind-key "C-x <down>" 'windmove-down)
 (bind-key "C-x <up>" 'windmove-up)
 
+(with-eval-after-load "dired"
+  (define-key dired-mode-map (kbd "<mouse-2>") #'dired-mouse-find-file)
+  (define-key dired-mode-map (kbd "<RET>") #'dired-find-file))
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
